0d64a0b08e8528b308ea6c780c8a5eb6d24150e3
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-02-28 11:40:58 -0800

    Implement Create an event

--- routes.js
+++ routes.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const pool = require('./connection');
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // landing 
